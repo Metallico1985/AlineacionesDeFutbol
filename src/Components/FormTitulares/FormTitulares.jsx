9403b5735dbb9b2ec5fa21c6f74f3b6b7de90ae2
andres cardona latorre <andrescardonalatorre@gmail.com>
2023-06-11 03:09:33 -0300

    nuevo estilo suplentes y ct

--- src/Components/FormTitulares/FormTitulares.jsx
+++ src/Components/FormTitulares/FormTitulares.jsx
@@ -16,13 +16,13 @@ function FormTitulares() {
   const handleNombre = (e) => { setNombre(e.target.value) }
   const handleNumero = (e) => { setNumero(e.target.value) }
   const agregarTitular = (t) => { setTitulares([...titulares, t]) }
-  const handleSubmit = (e) => { e.preventDefault(), agregarTitular(titular) ,console.log(titulares)}
- 
-  const [ct, setCt] = useState([])
-  const handleCuerpoTecnico = (i) => {setCt([...ct, i]), console.log(ct)}
+  const handleSubmit = (e) => { e.preventDefault(), agregarTitular(titular), console.log(titulares) }
 
-const [suplentes, setSuplentes] = useState([])
-const handleSuplentes = (s) => {setSuplentes([...suplentes, s])}
+  const [ct, setCt] = useState([])
+  const handleCuerpoTecnico = (i) => { setCt([...ct, i]), console.log(ct) }
+
+  const [suplentes, setSuplentes] = useState([])
+  const handleSuplentes = (s) => { setSuplentes([...suplentes, s]) }
 
   const titular = {
     nombre: nombre,
@@ -32,32 +32,35 @@ const handleSuplentes = (s) => {setSuplentes([...suplentes, s])}
 
   return (
     <div className='mainContainer'>
-      { titulares.length < 11 ? 
-      <div className='formContainer'>
-<h1>Ingresar plantel titular</h1>
-      <form onSubmit={handleSubmit} className='formIngreso'>
-        <input onChange={handleNombre} type="text" placeholder='Nombre' />
-        <input onChange={handleNumero} type="text" placeholder='Numero' />
-        <select onChange={handlePosicion} name='posiciones'>
-          <option disabled selected >Seleccionar posición</option>
-          <option value="golero">Golero</option>
-          <option value="lateralIzq">lateralIzquierdo</option>
-          <option value="lateralDer">lateralDerecho</option>
-          <option value="centralIzq">centralIzquierdo</option>
-          <option value="centralDer">centralDerecho</option>
-          <option value="volanteDer">volanteDerecho</option>
-          <option value="volanteIzq">volanteIzquierdo</option>
-          <option value="medioDer">mediocampista Derecho</option>
-          <option value="medioIzq">mediocampista Izquierdo</option>
-          <option value="delanteroDer">delanteroDerecho</option>
-          <option value="delanteroIzq">delanteroIzquierdo</option>
-        </select>
-        <button type='submit'>Agregar</button>
-      </form>
-</div>:<></> }
-      <FormSuplentes handleSuplentes = {handleSuplentes} lista={suplentes}/>
-      <FormEquipoTecnico handleCuerpoTecnico={handleCuerpoTecnico} lista ={ct} />
-      <Formacion cuerpoTecnico = {ct}  lista={titulares} suplentes={suplentes}/>
+
+      {titulares.length < 11 ?
+        <div className='formContainer'>
+          <h1>Ingresar plantel titular</h1>
+          <form onSubmit={handleSubmit} className='formIngreso'>
+            <input onChange={handleNombre} type="text" placeholder='Nombre' />
+            <input onChange={handleNumero} type="text" placeholder='Numero' />
+            <select onChange={handlePosicion} name='posiciones'>
+              <option disabled selected >Seleccionar posición</option>
+              <option value="golero">Golero</option>
+              <option value="lateralIzq">lateralIzquierdo</option>
+              <option value="lateralDer">lateralDerecho</option>
+              <option value="centralIzq">centralIzquierdo</option>
+              <option value="centralDer">centralDerecho</option>
+              <option value="volanteDer">volanteDerecho</option>
+              <option value="volanteIzq">volanteIzquierdo</option>
+              <option value="medioDer">mediocampista Derecho</option>
+              <option value="medioIzq">mediocampista Izquierdo</option>
+              <option value="delanteroDer">delanteroDerecho</option>
+              <option value="delanteroIzq">delanteroIzquierdo</option>
+            </select>
+            <button type='submit'>Agregar</button>
+          </form>
+        </div> :
+        <div className='leftSideMain'>
+          <FormSuplentes handleSuplentes={handleSuplentes} lista={suplentes} />
+          <FormEquipoTecnico handleCuerpoTecnico={handleCuerpoTecnico} lista={ct} />
+        </div>}
+      <Formacion cuerpoTecnico={ct} lista={titulares} suplentes={suplentes} />
     </div>
   )
 }
--- src/Components/Formacion/Formacion.jsx
+++ src/Components/Formacion/Formacion.jsx
@@ -1,46 +1,50 @@
 import React from 'react'
 import './Formacion.css'
-import {IoMdShirt} from 'react-icons/all'
+import { IoMdShirt } from 'react-icons/all'
 
 function Formacion(props) {
 
   return (
     <div className='mainFormacion'>
+      <div className='cancha'>
+        <div className='gridCancha'>
+          {props.lista.map((item) => (
+            <>
+              <div className={`${item.posicion} posicion`}>
+                <div><IoMdShirt size={50} color='' /></div>
+                <div>{item.nombre}</div>
+                <div>{item.numero}</div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
       <div className='ctYsup'>
-      <div className='suplentes'>
-    {props.suplentes.map((item)=>(
-      <>
-      <div>{item.nombre}</div>
-      <div>{item.posicion}</div>
-      </>
-    ))}
+        <h2>Suplentes</h2>
+        <div className='suplentes'>
+          {props.suplentes.map((item) => (
+            <>
+              <div className='txtSuplentes'>
+                <div className='supNumero txtColor'>{item.numero}</div>
+                <div className=' txtColor'>{item.nombre}</div>
+              </div>
+
+            </>
+          ))}
+        </div>
+        <h2>Cuerpo Técnico</h2>
+        <div className='cuerpoTecnico'>
+          {props.cuerpoTecnico.map((item) => (
+            <>
+              <div className='ctFuncion txtColor'>{item.funcion}</div>
+              <div className=' txtColor'>{item.nombre}</div>
+
+            </>
+          ))}
+
+        </div>
       </div>
 
-      <div className='cuerpoTecnico'>
-        {props.cuerpoTecnico.map((item)=>(
-        <>
-        <div>{item.nombre}</div>
-        <div>{item.funcion}</div>
-        </>
-        ))}
-      
-      </div>
-      </div>
-      <div className='cancha'>
-      <div className='gridCancha'>
-{props.lista.map((item)=>(
-  <>
-  <div className= {`${item.posicion} posicion`}> 
-  <div><IoMdShirt size={50} color=''/></div>
-  <div>{item.nombre}</div> 
-  <div>{item.numero}</div> 
-  
-  
-  </div>
-  </>
-))}
-      </div>
-      </div>
     </div>
   )
 }
